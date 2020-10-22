481136315bc574a04661caefd089dca79ff49110
Ruslan Rozhkov <bladteam@gmail.com>
2020-10-22 19:35:35 +0300

    Add power indicator to Engine

--- test/engine_spec.rb
+++ test/engine_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Engine do
     it "an engine has a power indicator from 0 to 100" do
       expect(0..100).to include(engine.power)
     end
+
+    it "set full power" do
+      engine.power = 100
+      expect(engine.power).to eq(100)
+    end
   end
 end
