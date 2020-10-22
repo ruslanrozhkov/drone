42c712df393a7d607d84f0b0e6d8aca1b35394d3
Ruslan Rozhkov <bladteam@gmail.com>
2020-10-22 19:14:24 +0300

    init commit

---
+++ test/engine_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/engine'
+
+RSpec.describe Engine do
+  let!(:engine) = described_class.new
+
+  describe "#power" do
+    it "an engine has a power indicator from 0 to 100" do
+      expect(0..100).to include(engine.power)
+    end
+  end
+end
